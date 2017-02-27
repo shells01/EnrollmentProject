7029ef5c59f6e6e5a6034b88b8cb65af380820b6
Shelly <shells01@users.noreply.github.com>
2017-02-27 11:00:30 -0800

    Add files via upload

--- Courses/Courses/Default.aspx
+++ Courses/Courses/Default.aspx
@@ -9,16 +9,31 @@
         .newStyle1 {
             font-family: Arial, Helvetica, sans-serif;
         }
+        .newStyle2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
     </style>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
     
-        <h3><span class="newStyle1">Student Enrollment Form</span></h3>
+        <h3><span class="newStyle2">Add Course Form</span></h3>
         <br />
+        Course
         Name:&nbsp;
         <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Instructor:&nbsp;
+        <asp:TextBox ID="instructorTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Department:&nbsp;
+        <asp:TextBox ID="departmentTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="submitButton" runat="server" Text="Add Course" />
         <br />
         <br />
         <asp:Label ID="resultLabel" runat="server"></asp:Label>
--- Courses/Courses/Default.aspx.designer.cs
+++ Courses/Courses/Default.aspx.designer.cs
@@ -31,6 +31,33 @@ namespace Courses {
         protected global::System.Web.UI.WebControls.TextBox nameTextBox;
         
         /// <summary>
+        /// instructorTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox instructorTextBox;
+        
+        /// <summary>
+        /// departmentTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox departmentTextBox;
+        
+        /// <summary>
+        /// submitButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button submitButton;
+        
+        /// <summary>
         /// resultLabel control.
         /// </summary>
         /// <remarks>
